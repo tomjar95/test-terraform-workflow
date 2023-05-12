79b8654252c4fd20ec588fa5adce8e6459fe24d8
TJ <107787727+tomjar95@users.noreply.github.com>
2023-05-12 16:25:17 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,9 @@
 variable "sciezka_do_pliku" {
-  default = local.welcome_path
+  default = "${path.module}/welcome.py"
   type = string
 }
 
 variable "zawartosc_pliku" {
   default = "print('Witaj!')"
   type = string
-}
-
-locals {
-  welcome_path = "${path.module}/welcome.py"
 }
