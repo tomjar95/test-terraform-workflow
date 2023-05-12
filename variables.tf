9b33fb222f310fea3b2c132c28163ab01ff1f64b
TJ <107787727+tomjar95@users.noreply.github.com>
2023-05-12 16:45:53 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "zawartosc_pliku" {
-  default = "python -c 'def greet():\n    print(\"Hello, world!\")\ngreet()'"
+  default = "def greet():\n    print(\"Hello, world!\")\ngreet()"
   type = string
 }
